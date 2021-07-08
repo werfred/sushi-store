fd1494f62d6cd92fee19a798cacb1cc16d8d0d3b
Oleg Didechkin <frikol3000@gmail.com>
2021-07-08 19:52:28 +0300

    New fields for sushi model. Added category model.

--- sushi_store/sushi/admin.py
+++ sushi_store/sushi/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import Sushi
+from django.db import models
+from .models import Sushi, Category
+from django import forms
 
-# Register your models here.
-admin.site.register(Sushi)
+
+class SushiModelForm(forms.ModelForm):
+
+    category = forms.CharField(max_length=100)
+
+    def __init__(self, *args, **kwargs):
+        super(SushiModelForm, self).__init__(*args, **kwargs)
+        self.fields['category'] = forms.ChoiceField(
+            choices=[(c.category_name.lower().replace(' ', '_'), c.category_name) for c in Category.objects.all()]
+        )
+    
+
+    class Meta:
+        model = Sushi
+        fields = ('name', 'category', 'description', 'image', 'quantity', 'price', 'discount')
+
+class SushiModelAdmin(admin.ModelAdmin):
+    form = SushiModelForm
+
+admin.site.register(Sushi, SushiModelAdmin)
+admin.site.register(Category)
--- sushi_store/sushi/serializers.py
+++ sushi_store/sushi/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Sushi
 
+
 class SushiSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Sushi
-        fields = ('pk','name', 'description', 'quantity', 'price')
+        fields = ('name', 'category', 'description', 'image', 'quantity', 'price', 'discount')
