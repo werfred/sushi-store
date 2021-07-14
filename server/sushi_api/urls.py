6328f5034138e4584ae69789f057eb4aede2eb94
Oleg Didechkin <frikol3000@gmail.com>
2021-07-14 19:02:38 +0300

    Added sort/filter endpoint

--- server/sushi_api/urls.py
+++ server/sushi_api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 
-
-application_name = 'sushi_api'
+app_name = 'sushi_api'
 
 urlpatterns = [
-    path('sushi', views.sushi_list),
-    path('<int:pk>', views.sushi_detail),
+    path('sushi', views.SushiList.as_view()),
+    path('<int:pk>', views.SushiDetail.as_view()),
 ]
--- server/sushi_store/settings.py
+++ server/sushi_store/settings.py
@@ -137,5 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+
+    'COERCE_DECIMAL_TO_STRING': False
 }
