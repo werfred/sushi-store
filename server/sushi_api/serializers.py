f76e469bb740e03f532eea5705edce9c09aa45a4
Oleg Didechkin <frikol3000@gmail.com>
2021-07-26 22:08:04 +0300

    Slug field added to serializer

--- server/sushi_api/serializers.py
+++ server/sushi_api/serializers.py
@@ -5,9 +5,10 @@ from sushi.models import Sushi
 class SushiSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.category_name')
     image = serializers.ImageField(
-            max_length=None, use_url=True
-        )
+        max_length=None, use_url=True
+    )
 
     class Meta:
         model = Sushi
-        fields = ('id', 'name', 'category_name', 'description', 'image', 'quantity', 'price', 'discount')
+        fields = ('id', 'slug', 'name', 'category_name',
+                  'description', 'image', 'quantity', 'price', 'discount')
