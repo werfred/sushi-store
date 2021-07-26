2171249ba80eee5842078d1dfc13a9cc906e0654
Oleg Didechkin <frikol3000@gmail.com>
2021-07-26 22:53:36 +0300

    Category name field format changed

--- server/sushi_api/serializers.py
+++ server/sushi_api/serializers.py
@@ -3,12 +3,12 @@ from sushi.models import Sushi
 
 
 class SushiSerializer(serializers.ModelSerializer):
-    category_name = serializers.CharField(source='category.category_name')
+    categoryName = serializers.CharField(source='category.category_name')
     image = serializers.ImageField(
         max_length=None, use_url=True
     )
 
     class Meta:
         model = Sushi
-        fields = ('id', 'slug', 'name', 'category_name',
+        fields = ('id', 'slug', 'name', 'categoryName',
                   'description', 'image', 'quantity', 'price', 'discount')
