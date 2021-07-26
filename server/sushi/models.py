4b5b583670ff309543213ae7787986f5822935fe
Oleg Didechkin <frikol3000@gmail.com>
2021-07-27 00:55:02 +0300

    Ingredients model, images, admin form added. Categories rus, ukr names added

--- server/sushi/models.py
+++ server/sushi/models.py
@@ -5,6 +5,8 @@ from django.conf import settings
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100)
+    category_name_ukr = models.CharField(max_length=100)
+    category_name_rus = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return self.category_name
@@ -12,11 +14,26 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+    image = models.ImageField(
+        upload_to=settings.MEDIA_ROOT_PATH + 'ingredients/')
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name_plural = "Ingredients"
+
+
 class Sushi(models.Model):
     name = models.CharField(max_length=100, null=False)
     image = models.ImageField(upload_to=settings.MEDIA_ROOT_PATH, null=True)
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name='category')
+    ingredients = models.ManyToManyField(
+        Ingredient, through='SetOfIngredients')
     description = models.TextField(default='')
     quantity = models.PositiveSmallIntegerField(default=1)
     price = models.DecimalField(default=0.0, max_digits=6, decimal_places=2)
@@ -24,8 +41,8 @@ class Sushi(models.Model):
     slug = models.SlugField(max_length=255)
 
     def save(self, *args, **kwargs):
-        img_path = self.image.url.replace(
-            settings.MEDIA_URL + settings.MEDIA_ROOT_PATH, '').split('.')[0]
+        img_path = self.image.url.replace(settings.MEDIA_URL, '').replace(
+            settings.MEDIA_ROOT_PATH, '').split('.')[0].split('_')[0]
         if not self.slug:
             self.slug = slugify(img_path)
         super(Sushi, self).save(*args, **kwargs)
@@ -42,3 +59,10 @@ class Sushi(models.Model):
     class Meta:
         verbose_name_plural = "Sushi"
 
+
+class SetOfIngredients(models.Model):
+    sushi = models.ForeignKey(Sushi, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.ingredient.name
--- server/sushi_api/serializers.py
+++ server/sushi_api/serializers.py
@@ -4,19 +4,27 @@ from sushi.models import Sushi, Category
 
 class SushiSerializer(serializers.ModelSerializer):
     categoryName = serializers.CharField(source='category.category_name')
+    categoryNameRus = serializers.CharField(
+        source='category.category_name_rus')
+    categoryNameUkr = serializers.CharField(
+        source='category.category_name_ukr')
     image = serializers.ImageField(
         max_length=None, use_url=True
     )
 
     class Meta:
         model = Sushi
-        fields = ('id', 'slug', 'name', 'categoryName',
+        fields = ('id', 'slug', 'name', 'categoryName', 'categoryNameRus', 'categoryNameUkr',
                   'description', 'image', 'quantity', 'price', 'discount')
 
 
 class CategorySerializer(serializers.ModelSerializer):
     categoryName = serializers.CharField(source='category_name')
+    categoryNameRus = serializers.CharField(
+        source='category_name_rus')
+    categoryNameUkr = serializers.CharField(
+        source='category_name_ukr')
 
     class Meta:
         model = Category
-        fields = ('id', 'categoryName')
+        fields = ('id', 'categoryName', 'categoryNameRus', 'categoryNameUkr')
