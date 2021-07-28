469930a1b6802fff53dbefbf78951bb7344fb954
Oleg Didechkin <frikol3000@gmail.com>
2021-07-28 18:32:37 +0300

    Prev, next slug fields added

--- server/sushi_api/serializers.py
+++ server/sushi_api/serializers.py
@@ -2,16 +2,24 @@ from rest_framework import serializers
 from sushi.models import Sushi, Category, Ingredient
 
 
-class IngredientSerializer(serializers.ModelSerializer):
+class ReadOnlyModelSerializer(serializers.ModelSerializer):
+    def get_fields(self, *args, **kwargs):
+        fields = super().get_fields(*args, **kwargs)
+        for field in fields:
+            fields[field].read_only = True
+        return fields
+
+
+class IngredientSerializer(ReadOnlyModelSerializer):
     nameRus = serializers.CharField(source='name_rus')
     nameUkr = serializers.CharField(source='name_ukr')
 
     class Meta:
         model = Ingredient
-        fields = ['name', 'nameRus', 'nameUkr', 'image']
+        fields = '__all__'
 
 
-class SushiSerializer(serializers.ModelSerializer):
+class SushiSerializer(ReadOnlyModelSerializer):
     categoryName = serializers.CharField(source='category.category_name')
     categoryNameRus = serializers.CharField(
         source='category.category_name_rus')
@@ -22,13 +30,29 @@ class SushiSerializer(serializers.ModelSerializer):
     )
     ingredients = IngredientSerializer(many=True)
 
+    next_slug = serializers.SerializerMethodField('get_next_slug')
+    prev_slug = serializers.SerializerMethodField('get_prev_slug')
+
+    def get_next_slug(self, sushi):
+        try:
+            slug = Sushi.objects.get(pk=sushi.id + 1).slug
+        except Sushi.DoesNotExist:
+            slug = Sushi.objects.order_by('id').first().slug
+        return slug
+
+    def get_prev_slug(self, sushi):
+        try:
+            slug = Sushi.objects.get(pk=sushi.id - 1).slug
+        except Sushi.DoesNotExist:
+            slug = Sushi.objects.order_by('id').last().slug
+        return slug
+
     class Meta:
         model = Sushi
-        fields = ('id', 'slug', 'name', 'categoryName', 'categoryNameRus', 'categoryNameUkr',
-                  'description', 'image', 'quantity', 'price', 'discount', 'ingredients')
+        fields = '__all__'
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySerializer(ReadOnlyModelSerializer):
     categoryName = serializers.CharField(source='category_name')
     categoryNameRus = serializers.CharField(
         source='category_name_rus')
@@ -37,4 +61,4 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ('id', 'categoryName', 'categoryNameRus', 'categoryNameUkr')
+        fields = '__all__'
